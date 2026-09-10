8d9ded90fed7e38a9f1e8b618ce805a7ca705af6
Jhone-cmd <jhone.1941@gmail.com>
2026-09-10 16:12:55 -0300

    feat: intercepting routes - logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ProxyModule } from './proxy/proxy.module.js';
 import { MiddlewareModule } from './middleware/middleware.module.js';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware.js';
 
 @Module({
   imports: [
@@ -23,4 +24,8 @@ import { MiddlewareModule } from './middleware/middleware.module.js';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
