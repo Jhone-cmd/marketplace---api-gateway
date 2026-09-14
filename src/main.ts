3aec0f92aee99db9ec1c2ccf2b0479bb9c8c0748
Jhone-cmd <jhone.1941@gmail.com>
2026-09-14 16:27:00 -0300

    feat: advanced HELMET settings

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
 
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          imgSrc: ["'self'", 'data:', 'https:'],
+        },
+      },
+      crossOriginEmbedderPolicy: false,
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true,
+      },
+    }),
+  );
+
   app.enableCors({
     origin: (origin: any, cb: any) => {
       if (!origin) return cb(null, true);
