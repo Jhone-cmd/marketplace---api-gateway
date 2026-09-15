7d7e105280cfa5b0422a32308acebb5050b39033
Jhone-cmd <jhone.1941@gmail.com>
2026-09-15 15:14:02 -0300

    feat: implementation of the authentication service – validateJwtToken method

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly jwtService: JwtService, private readonly configService: ConfigService) { }
+    constructor(private readonly jwtService: JwtService, private readonly httpService: HttpService) { }
 
-    validateJwtToken() { }
+    validateJwtToken(token: string): Promise<any> {
+        try {
+            return this.jwtService.verify(token);
+        } catch (error) {
+            throw new UnauthorizedException('Invalid JWT token');
+        }
+    }
     validateSessionToken() { }
     login() { }
     register() { }
