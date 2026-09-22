4d0ef1c89f3455cb4d62a53ff371f4fe0b73521d
Jhone-cmd <jhone.1941@gmail.com>
2026-09-22 17:01:51 -0300

    feat: implementing the login route in the auth controller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from '../service/auth.service.js';
 
 @ApiTags("Authentication")
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
+
+    @Post("login")
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: "User login" })
+    @ApiResponse({ status: 200, description: "Login successfully" })
+    @ApiResponse({ status: 401, description: "Invalid credentials" })
+    async login(@Body() loginDto: { email: string; password: string }) {
+        return this.authService.login(loginDto);
+    }
 }
