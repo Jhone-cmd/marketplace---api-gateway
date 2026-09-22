8ca239c010ce8605e10a85ff50b62ac7160bf4df
Jhone-cmd <jhone.1941@gmail.com>
2026-09-22 16:58:09 -0300

    feat: created auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './service/auth.service.js';
+import { AuthController } from './controllers/auth.controller.js';
 
 @Module({
     imports: [
@@ -20,5 +21,6 @@ import { AuthService } from './service/auth.service.js';
     ],
     providers: [AuthService],
     exports: [],
+    controllers: [AuthController],
 })
 export class AuthModule { }
